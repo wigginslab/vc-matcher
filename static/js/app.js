7d7c0217f00aaead625ceb3be151b0c1f1fddb4c
jennifer <jrubinovitz@gmail.com>
2013-12-28 22:22:59 -0500

    show errors

--- static/js/app.js
+++ static/js/app.js
@@ -1,21 +1,32 @@
 $("#submit-desc").click(function(){
 	desc = $("#description").val();
+	$("#desc-error").hide();
+	$("#desc-warning").hide();
 	page = 1;
-	$.ajax({
-		url:"/get/vcs/"+page,
-		method:"POST",
-		data:"description="+desc,
-		success:function(data){
-			_.templateSettings.variable = "rc";
+	// description too short
+	if (desc.split(" ").length < 3){
+		$("#desc-parent").addClass("has-warning");
+		$("#desc-warning").show();
+	}
+	else{
+		$.ajax({
+			url:"/get/vcs/"+page,
+			method:"POST",
+			data:"description="+desc,
+			success:function(data){
+				_.templateSettings.variable = "rc";
 
-			var template = _.template(
-            	$( "script.template" ).html()
-        	);
-			console.log(data);
-        	$( "#vc-list" ).html(
-            template(data)
-       		 );
-        	page = page + 1;
-		}
-	});
+				var template = _.template(
+	            	$( "script.template" ).html()
+	        	);
+				console.log(data);
+	        	$( "#vc-list" ).html(
+	            template(data)
+	       		 );
+	        	page = page + 1;
+			},
+			error:function(){
+				$("#desc-error").show();
+			}
+		});
 });
